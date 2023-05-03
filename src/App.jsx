18469843f6264e7d5e366fb49977c3f1e4b6bd0e
ras1k <rasikabdullah@gmail.com>
2023-05-03 13:06:21 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
-    <div>
-
+    <div className='container'>
+      <Header></Header>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import './Header.css';
 const Header = () => {
     return (
         <div>
-            
+            <div className='d-flex justify-content-between'>
+                <h2>Team Campus</h2>
+                <img src="../../../public/dp.png" className='' alt="" />
+            </div>
+            <hr />
         </div>
     );
 };
