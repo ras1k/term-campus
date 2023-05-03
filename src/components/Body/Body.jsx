59d3fcbe1d051dd2a2cbb45754c2c1ec8a8d8095
ras1k <rasikabdullah@gmail.com>
2023-05-03 17:56:35 +0600

    images added

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Body.css';
 import Main from '../Main/Main';
 import Sidecart from '../Sidecart/Sidecart';
 
 const Body = () => {
+    const [cards, setCards] = useState([]);
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCards(data))
+    }, []);
     return (
         <div>
             <div className='row'>
                 <div className='col-md-8'>
-                    <Main></Main>
+                    {
+                        cards.map(card =>
+                            <Main
+                                key={card.id}
+                                card={card}
+                            ></Main>)
+                    }
+
                 </div>
                 <div className='col-md-4 card'>
                     <Sidecart></Sidecart>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import './Main.css'
 
-const Main = () => {
-    
+const Main = (props) => {
+    const {name, title, date, read, cover_img, person_img} = props.card;
     return (
-        <div className='card'>
+        <div className='card mb-3'>
             <div className="blogs-container">
                 <div className="blog-cart">
-                    <img src="../../../public/cr7.jpg" alt="" className="banner w-100 mb-2" />
+                    <img src={cover_img} alt="" className="banner w-100 mb-2" />
                 </div>
                 <div className='title-name d-flex justify-content-between'>
-                    <div className='d-flex gap-3'>
+                    <div className='person d-flex gap-3'>
                         <div>
-                            <img src="../../../public/dp.png" alt="" />
+                            <img src={person_img} alt="" />
                         </div>
                         <div>
-                            <h5 className='name'>Name</h5>
-                            <p><small>date</small></p>
+                            <h5 className='name'>Name : {name}</h5>
+                            <p><small>{date}</small></p>
                         </div>
                     </div>
                     <div>
-                        <p>5 mins read</p>
+                        <p>{read}</p>
                     </div>
                 </div>
                 <div className='Title'>
-                    <h3>Title</h3>
+                    <h3>{title}</h3>
                 </div>
                 <div>
                     <a href="">Mark As Read</a>
